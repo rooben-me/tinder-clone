0c8b9ee254131aaebf9fa294e6f572174a30be81
ruban <mridiot@protonmail.com>
2020-09-17 15:03:18 +0530

    Updates

---
+++ precache-manifest.5b150df74c0626fce0251bb7dc698fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37e0ba90075014f0ba2897d9b0d13fa",
+    "url": "/tinder-clone/index.html"
+  },
+  {
+    "revision": "ebb0b284d3d4d393215a",
+    "url": "/tinder-clone/static/css/main.5fe9b76a.chunk.css"
+  },
+  {
+    "revision": "85e063456832d2bb89da",
+    "url": "/tinder-clone/static/js/2.0bb7621f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tinder-clone/static/js/2.0bb7621f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb0b284d3d4d393215a",
+    "url": "/tinder-clone/static/js/main.32c618d8.chunk.js"
+  },
+  {
+    "revision": "de86c0115f6746d23e21",
+    "url": "/tinder-clone/static/js/runtime-main.acd27e62.js"
+  }
+]);
